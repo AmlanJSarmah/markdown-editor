a4b8bdef26b361a365397ecee84083aabfd6b7e0
ajsarmah <devajsarmah@gmail.com>
2022-02-05 20:47:17 +0530

    Created the Navbar for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Header} from './components/Header'
 
 const App:React.FC = () => {
   return (
     <div className="App">
-      Hello World
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+	return (
+		<nav className="navbar navbar-dark bg-dark">
+			<div className="container-fluid">
+				<span className="navbar-brand mb-0 h1">Markdown Editor</span> 
+			</div>
+		</nav>
+	);
+};

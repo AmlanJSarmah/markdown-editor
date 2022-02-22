133346eda11e3c656df5be9400990c91db1901a4
A.J.Sarmah <devajsarmah@gmail.com>
2022-02-22 17:47:51 +0530

    Added basic compiler functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,13 @@
 export const setStorage = (text: string | null) => {
-  if(text) localStorage.setItem('Data',text)
-  else localStorage.setItem('Data',"")
-}
+  if (text) localStorage.setItem("Data", text);
+  else localStorage.setItem("Data", "");
+};
 
 export const getStorage = () => {
-    if(localStorage.length === 0) return null
-    else return localStorage.getItem('Data')
-}
+  if (localStorage.length === 0) return null;
+  else return localStorage.getItem("Data");
+};
+
+export const markdownCreator = (text: string) => {
+  const textArray = text.split(" ");
+};
